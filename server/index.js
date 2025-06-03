8e9af598ed8249c0ebf847ebc27d77b21ca415eb
Iren0708 <sti4070802@mail.ru>
2025-06-03 11:24:24 +0300

    main

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json()); // Для обработки JSON-тела запрос
 
     const {default: UserController} = await import('./controller/UserController.js');
     app.use('/api/users', UserController);
-    
+
 
     app.listen(3000, () => { //Подключение сервера
         console.log('Server is running on port 3000');
